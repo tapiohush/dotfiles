a1146ad831d6368f7b346fdff62f316249c9443a
Robin Folland <tapiohush@gmail.com>
2010-05-13 02:41:41 +0200

    Fix.

--- mklinks.sh
+++ mklinks.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 for file in .*; do 
-	if [ ! -d $file ]; then 
-		if [ ! -e $HOME/$file ]; then
-			ln -s $HOME/dotfiles/$file $HOME;
-		else 
-			print "$HOME/$file exists. Skipping";
-			continue;
-		fi
-	fi
+    if [ ! -d $file ]; then 
+        if [ ! -e $HOME/$file ]; then
+            ln -s $HOME/dotfiles/$file $HOME;
+        fi
+    fi
 done
 
