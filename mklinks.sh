b1f07c79e3ffb8ecba30df0b4a3d0055c94bdc16
Robin Folland <tapiohush@gmail.com>
2010-05-13 02:26:32 +0200

    edit script

--- mklinks.sh
+++ mklinks.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-for file in .*; do if [ ! -d $file ]; then ln -s $HOME/dotfiles/$file ~; fi; done
+
+for file in .*; do 
+	if [ ! -d $file ]; then 
+		if [ ! -e $HOME/$file ]; then
+			ln -s $HOME/dotfiles/$file $HOME;
+		else 
+			print "$HOME/$file exists. Skipping";
+			continue;
+		fi
+	fi
+done
 
